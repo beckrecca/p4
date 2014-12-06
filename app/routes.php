8e554a7aae25684031cc4d2ecff5231d0c2059f3
Rebecca Doris <rebecca.doris42@gmail.com>
2014-12-06 16:16:10 -0500

    mysql-test

--- app/routes.php
+++ app/routes.php
@@ -71,3 +71,16 @@ Route::get('/logout', function() {
     return Redirect::to('/');
 
 });
+
+Route::get('mysql-test', function() {
+
+    # Print environment
+    echo 'Environment: '.App::environment().'<br>';
+
+    # Use the DB component to select all the databases
+    $results = DB::select('SHOW DATABASES;');
+
+    # If the "Pre" package is not installed, you should output using print_r instead
+    echo Pre::render($results);
+
+});
