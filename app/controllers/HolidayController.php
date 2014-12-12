1e6b35a460e6a24edb8e71ed6d62842471fac270
Rebecca Doris <rebecca.doris42@gmail.com>
2014-12-11 21:59:25 -0500

    fixed validation bug

--- app/controllers/HolidayController.php
+++ app/controllers/HolidayController.php
@@ -66,7 +66,7 @@ class HolidayController extends BaseController
             'title' => 'required|max:128',
             'description' => 'max:160',
             'location' => 'required',
-            'date' => 'required|date|date_format:"yyyy-mm-dd"',
+            'date' => 'required|date|date_format:"Y-m-d"',
             'time' => 'between:1,12|numeric',
             'm' => 'between:0,1|numeric',
 
@@ -128,7 +128,7 @@ class HolidayController extends BaseController
             'title' => 'required|max:128',
             'description' => 'max:160',
             'location' => 'required',
-            'date' => 'required|date|date_format:"yyyy-mm-dd"',
+            'date' => 'required|date|date_format:"Y-m-d"',
             'time' => 'between:1,12|numeric',
             'm' => 'between:0,1|numeric',
 
