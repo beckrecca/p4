048c90f226b8700ce6171105ae127cf5c614ad14
Rebecca Doris <rebecca.doris42@gmail.com>
2015-01-22 21:51:42 -0500

    Dorisdays is moving!

--- app/routes.php
+++ app/routes.php
@@ -16,52 +16,6 @@ Route::get('/', function()
 	return View::make('moved');
 });
 
-# EVENTS
-
-Route::get('/events', 'HolidayController@index');
-
-Route::get('/events/all', 'HolidayController@all');
-
-Route::get('/events/create', 'HolidayController@create');
-
-Route::post('/events/create', 'HolidayController@handleCreate');
-
-Route::get('/events/view/{id}', 'HolidayController@view');
-
-Route::get('/events/edit/{id}', 'HolidayController@edit');
-
-Route::post('/events/edit', 'HolidayController@handleEdit');
-
-Route::get('/events/delete/{id}', 'HolidayController@delete');
-
-Route::post('/events/delete', 'HolidayController@handleDelete');
-
-# COMMENTS
-
-Route::get('/comments', 'CommentController@comment_index');
-
-Route::get('/comments/{id}', 'CommentController@create');
-
-Route::post('/comments', 'CommentController@handleCreate');
-
-# USERS
-
-Route::get('/V5RDN82zU67F8oG88x4q', 'UserController@getSignup');
-
-Route::post('/V5RDN82zU67F8oG88x4q', 'UserController@postSignup');
-
-Route::get('/login', 'UserController@getLogin');
-
-Route::post('/login', 'UserController@postLogin');
-
-Route::get('/profile', 'UserController@view');
-
-Route::get('/edit_profile', 'UserController@edit');
-
-Route::post('/edit_profile', 'UserController@handleEdit');
-
-Route::get('logout', 'UserController@logout');
-
 # TESTING
 
 Route::get('whoops', function() {
